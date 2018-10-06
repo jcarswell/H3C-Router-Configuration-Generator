82a66e813f796a1cd8c1f84445b14e94b4590c4f
Josh Carswell <Josh.Carswell@thecarswells.ca>
2018-10-06 10:35:26 -0600

    added acl object class

--- build.py
+++ build.py
@@ -44,3 +44,54 @@ def main():
         elif obj_csv != None:
             objCfg(c,acl_cfg)
 
+def intCfg(config,csvFile):
+    with open(csvFile) as data:
+        csv_data = csv.DictReader(data)
+        for l in csv_data:
+
+class Acl():
+    rules = {}
+    name = None
+    acl_num = None
+
+    def __init__(self,n,r=None):
+        self.name = n
+        self.acl_num = r
+    
+    def add(self,data):
+        if type(acl) != 'dict':
+            raise TypeError('data must be a dict')
+
+        try:
+            rule_num = int(data['num'])
+        else:
+            rule_num = 0
+        except ValueError:
+            rule_num = 0
+
+        while true:
+            if rule_num in self.rules:
+                rule_num = rule_num + 5
+            else:
+                break
+        self.rules[rule_num] = {}
+        for x in data:
+            if x == 'num' or 
+                x == 'name' or 
+                x == 'ipv':
+                pass
+            else:
+                self.rules[rule_num] = {x:data[x]}
+
+
+    def output(self,f):
+        pass
+
+class Acl6(Acl):
+    def output(self,f):
+        pass
+
+
+
+
+
